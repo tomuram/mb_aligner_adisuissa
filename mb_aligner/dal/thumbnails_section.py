3dc3dc6180a7c1e239ee401a8d9aa6d567d95102
Adi Suissa-Peleg <adisuis@seas.harvard.edu>
2018-05-24 09:27:01 -0400

    Adding thumnails section and tiles

--- mb_aligner/dal/thumbnails_section.py
+++ mb_aligner/dal/thumbnails_section.py
@@ -1,15 +1,16 @@
 from collections import defaultdict
 import os
 import json
-from mb_aligner.dal.tile import Tile
-from mb_aligner.dal.mfov import Mfov
 import csv
 import numpy as np
 import cv2
+from mb_aligner.dal.section import Section
+from mb_aligner.dal.thumbnails_tile import ThumbnailsTile
 
-class Section(object):
+class ThumbnailsSection(object):
     """
-    Represents a single section (at least one mfov) in the system
+    Represents a single section (at least one mfov) of thumbnails in the system.
+    Each tile is actually the "stitched" mfov (using the thumbnail coordinates)
     """
 
     def __init__(self, mfovs_dict, **kwargs):
@@ -27,18 +28,6 @@ class Section(object):
             
 
     @classmethod
-    def create_from_tilespec(cls, tilespec):
-        """
-        Creates a section from a given tilespec
-        """
-        per_mfov_tiles = defaultdict(list)
-        for tile_ts in tilespec:
-            per_mfov_tiles[tile_ts["mfov"]].append(Tile.create_from_tilespec(tiles_ts))
-        layer = int(tilespec[0]["layer"])
-        all_mfovs = {mfov_idx:Mfov(mfov_tiles_list) for mfov_idx, mfov_tiles_list in per_mfov_tiles.items()}
-        return Section(all_mfovs, kwargs={'layer':layer})
-
-    @classmethod
     def _parse_coordinates_file(cls, input_file):
         # Read the relevant mfovs tiles locations
         images_dict = {}
@@ -53,7 +42,7 @@ class Section(object):
                 if not (img_fname.split('/')[0]).isdigit():
                     # skip the row
                     continue
-                img_sec_mfov_beam = '_'.join(img_fname.split('/')[-1].split('_')[:3])
+                img_sec_mfov_beam = '_'.join(img_fname.split('/')[-1].split('_')[1:4])
                 # Make sure that no duplicates appear
                 if img_sec_mfov_beam not in images_dict.keys():
                     images.append(img_fname)
@@ -79,21 +68,22 @@ class Section(object):
 
         return images, np.array(x), np.array(y)
 
-    @classmethod
-    def create_from_full_image_coordinates(cls, full_image_coordinates_fname, layer, tile_size=None):
-        """
-        Creates a section from a given full_image_coordinates filename
-        """
-        images, x_locs, y_locs = Section._parse_coordinates_file(full_image_coordinates_fname)
-        assert(len(images) > 0)
-        section_folder = os.path.dirname(full_image_coordinates_fname)
 
-        # Update tile_size if needed
-        if tile_size is None:
+    @classmethod
+    def create_from_full_thumbnail_coordinates(cls, full_thumbnail_coordinates_fname, layer, thumb_tile_size=None):
+        """
+        Creates a section from a given full_thumbnail_coordinates filename
+        """
+        images, x_locs, y_locs = ThumbnailsSection._parse_coordinates_file(full_thumbnail_coordinates_fname)
+        assert(len(images) > 0)
+        section_folder = os.path.dirname(full_thumbnail_coordinates_fname)
+
+        # Update thumb_tile_size if needed
+        if thumb_tile_size is None:
             # read the first image
             img_fname = os.path.join(section_folder, images[0])
             img = cv2.imread(img_fname, 0)
-            tile_size = img.shape
+            thumb_tile_size = img.shape
 
         # normalize the locations of all the tiles (reset to (0, 0))
         x_locs -= np.min(x_locs)
@@ -101,19 +91,25 @@ class Section(object):
 
 
         # Create all the tiles
-        per_mfov_tiles = defaultdict(list)
+        per_mfov_tiles_fnames = defaultdict(list)
+        per_mfov_tiles_locs = defaultdict(list)
         for tile_fname, tile_x, tile_y, in zip(images, x_locs, y_locs):
             tile_fname = os.path.join(section_folder, tile_fname)
             # fetch mfov_idx, and tile_idx
             split_data = os.path.basename(tile_fname).split('_')
-            mfov_idx = int(split_data[1])
-            tile_idx = int(split_data[2])
-            print('adding mfov_idx %d, tile_idx %d' % (mfov_idx, tile_idx))
-            tile = Tile.create_from_input(tile_fname, tile_size, (tile_x, tile_y), layer, mfov_idx, tile_idx)
-            per_mfov_tiles[mfov_idx].append(tile)
+            mfov_idx = int(split_data[2])
+            #tile_idx = int(split_data[3])
+            #print('adding mfov_idx %d, tile_idx %d' % (mfov_idx, tile_idx))
+            per_mfov_tiles_fnames[mfov_idx].append(tile_fname)
+            per_mfov_tiles_locs[mfov_idx].append(np.array([tile_x, tile_y]))
             
-        all_mfovs = {mfov_idx:Mfov(mfov_tiles_list) for mfov_idx, mfov_tiles_list in per_mfov_tiles.items()}
-        return Section(all_mfovs, kwargs={'layer':layer})
+
+        all_mfovs = {}
+        for mfov_idx in per_mfov_tiles_fnames.keys():
+            print("Creating mfov thumbnails stitched tile: {}".format(mfov_idx))
+            mfov_thumbs_tile = ThumbnailsTile.create_from_input(per_mfov_tiles_fnames[mfov_idx], thumb_tile_size, per_mfov_tiles_locs[mfov_idx], layer, mfov_idx)
+            all_mfovs[mfov_idx] = mfov_thumbs_tile
+        return ThumbnailsSection(all_mfovs, kwargs={'layer':layer})
 
 
 
@@ -124,28 +120,28 @@ class Section(object):
         """
         return self._layer
 
-    @property
-    def tilespec(self):
-        """
-        Returns a tilespec representation of the mfov
-        """
-        ret = []
-        # Order the mfovs by the mfov index
-        sorted_mfov_idxs = sorted(self._mfovs_dict.keys())
-        for mfov_idx in sorted_mfov_idxs:
-            ret.extend(self._mfovs_dict[mfov_idx].tilespec)
-        return ret
-
-    def save_as_json(self, out_fname):
-        """
-        Saves the section as a tilespec
-        """
-        with open(out_fname, 'w') as out_f:
-            json.dump(self.tilespec, out_f, sort_keys=True, indent=4)
+#     @property
+#     def tilespec(self):
+#         """
+#         Returns a tilespec representation of the mfov
+#         """
+#         ret = []
+#         # Order the mfovs by the mfov index
+#         sorted_mfov_idxs = sorted(self._mfovs_dict.keys())
+#         for mfov_idx in sorted_mfov_idxs:
+#             ret.extend(self._mfovs_dict[mfov_idx].tilespec)
+#         return ret
+# 
+#     def save_as_json(self, out_fname):
+#         """
+#         Saves the section as a tilespec
+#         """
+#         with open(out_fname, 'w') as out_f:
+#             json.dump(self.tilespec, out_f, sort_keys=True, indent=4)
 
     def mfovs(self):
         '''
-        A generator that iterates over all the mfovs in the section
+        A generator that iterates over all the mfovs (tiles in our case) in the section
         '''
         mfov_keys = sorted(self._mfovs_dict.keys())
         for mfov_idx in mfov_keys:
@@ -153,19 +149,15 @@ class Section(object):
 
     def tiles(self):
         '''
-        A generator that iterates over all the tiles in the section
+        A generator that iterates over all the tiles (mfovs in our case) in the section
         '''
-        for mfov in self.mfovs():
-            for tile in mfov.tiles():
-                yield tile
+        return self.mfovs()
 
         
 
 if __name__ == '__main__':
-    section = Section.create_from_full_image_coordinates('/n/home10/adisuis/Harvard/git/rh_aligner/tests/ECS_test9_cropped/images/010_S10R1/full_image_coordinates.txt', 5)
+    section = ThumbnailsSection.create_from_full_thumbnail_coordinates('/n/lichtmanfs2/Alex/EM/ROI2_w03/W03_H01_ROI2_20180113_00-26-33/021_S56R1/full_thumbnail_coordinates.txt', 56)
 
     for mfov in section.mfovs():
         print("Mfov idx: %d" % mfov.mfov_index)
-    for tile in section.tiles():
-        print("Tile idx: %d (mfov %d)" % (tile.tile_index, tile.mfov_index))
 
